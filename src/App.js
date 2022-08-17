031a6c404890530ae2b50d6276bc93cdb7a4e7b4
saideepthi <saideepthi2001@gmail.com>
2022-08-17 23:36:38 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
+const App = () => {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [editId, setEditId] = useState(0);
 
-function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (editId) {
+      // whole id+todo
+      const editedTodo = todos.find((t) => t.id === editId);
+      const updatedTodos = todos.map((t) =>
+        t.id === editedTodo.id
+          ? (t = { id: t.id, todo })
+          : (t = { id: t.id, todo: t.todo })
+      );
+      setTodos(updatedTodos);
+      setEditId(0);
+      setTodo("");
+      return;
+    }
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+      setTodo("");
+    }
+  };
+
+  const handleDelete = (id) => {
+    const newList = todos.filter((to) => to.id !== id);
+    setTodos(newList);
+  };
+
+  const handleEdit = (id) => {
+    const editTodo = todos.find((x) => x.id === id);
+    setTodo(editTodo.todo);
+    setEditId(id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <TodoForm
+          handleSubmit={handleSubmit}
+          todo={todo}
+          setTodo={setTodo}
+          editId={editId}
+        />
+
+        <TodoList
+          handleDelete={handleDelete}
+          handleEdit={handleEdit}
+          todos={todos}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
